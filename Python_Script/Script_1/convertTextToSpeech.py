1e42af5eb0eb690c3816c89552a15d2d880a381c
Parthmasala <202001244@daiict.ac.in>
2022-01-16 14:46:26 +0530

    add

--- Python_Script/Script_1/convertTextToSpeech.py
+++
@@ -1,7 +0,0 @@
-#To convert text to speech
-# pip install gTTS to install this module
-from gtts import gTTS
-  
-speech = input("Enter text to be convert in speech(English) : ")
-myobj = gTTS(text=speech, lang = 'en' , slow=False)
-myobj.save("speech.mp3")
